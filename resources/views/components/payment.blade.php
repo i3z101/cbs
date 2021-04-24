f250ac4f2e8b46d8dc8863c9c2a8935be6929d61
i3z101 <abofahad.en@gmail.com>
2021-04-25 07:43:00 +0800

    first commit

--- resources/views/components/payment.blade.php
+++ resources/views/components/payment.blade.php
@@ -1,5 +1,5 @@
 <form accept-charset="UTF-8" action="https://api.moyasar.com/v1/payments.html" method="POST" class="flex flex-col w-1/2 m-auto mt-10">
-    <input type="hidden" name="callback_url" value="http://localhost:8000/complete-payment" />
+    <input type="hidden" name="callback_url" value="https://cbsapp.herokuapp.com/complete-payment" />
     <input type="hidden" name="publishable_api_key" value="pk_test_JkfSi7UmHdMuNrpZYrW8P5QKsJuiYs6wnWe1K3H8" />
     <input type="hidden" name="amount" value="{{$amount}}" />
     <input type="hidden" name="source[type]" value="creditcard" />
