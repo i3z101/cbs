ce7d101acf41668a8f160028a94966ae20e9d46e
i3z101 <abofahad.en@gmail.com>
2021-04-25 08:37:16 +0800

    first commit

--- resources/views/user/movieDetail.blade.php
+++ resources/views/user/movieDetail.blade.php
@@ -43,12 +43,10 @@
     @endif
     @foreach ($cinemas as $cinema)
       @if($cinema->cId == $movie->cinemaId)
-        <div class="flex flex-col justify-center items-center">
-            @include('components.modal', [
-                'movieName' => $movie->mName,
-                'cinemaName'=> $cinema->cName
-            ])
-        </div>
+        @include('components.modal', [
+            'movieName' => $movie->mName,
+            'cinemaName'=> $cinema->cName
+        ])
       @endif
     @endforeach
 </div>
